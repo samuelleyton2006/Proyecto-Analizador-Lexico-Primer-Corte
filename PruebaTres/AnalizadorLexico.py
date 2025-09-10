0fa69f1aad8e7acdfff3ebe34bdf09cebb5a3c6e
rokko <wsteban@hotmail.com>
2025-09-10 09:46:44 -0500

    el mejor es el tercero

--- PruebaTres/AnalizadorLexico.py
+++ PruebaTres/AnalizadorLexico.py
@@ -3,7 +3,7 @@ PALABRAS_RESERVADAS = {
     'False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def',
     'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import',
     'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try',
-    'while', 'with', 'yield', 'print'
+    'while', 'with', 'yield', 'print','object','str','int','float','bool','list','dict','set','tuple'
 }
 
 OPERADORES_DOBLES = {
@@ -17,8 +17,23 @@ OPERADORES_SIMPLES = {
     ')': 'tk_par_der', '{': 'tk_llave_izq', '}': 'tk_llave_der', '[': 'tk_corchete_izq',
     ']': 'tk_corchete_der', '<': 'tk_menor', '>': 'tk_mayor'
 }
+def es_letra(cadena: str)-> bool:
+    if len(cadena)==0:
+        return False
+    return ('a' <= cadena <= 'z') or ('A' <= cadena <= 'Z')
 
-# --- Automatizadores por token ---
+def es_digito(cadena: str)-> bool:
+    if len(cadena)==0:
+        return False
+    return '0' <= cadena <= '9'
+
+def es_alfnum(cadena: str)-> bool:
+    return es_letra(cadena) or es_digito(cadena)
+
+def es_blanco(cadena: str)-> bool:
+    return cadena in [' ', '\t', '\n', '\r', '\v', '\f']
+
+
 class AutomataCadena:
     def match(self, linea, pos):
         if linea[pos] not in ['"',"'"]: return None
@@ -46,22 +61,22 @@ class AutomataEntero:
                 if linea[i] in '+-':
                     lexema += linea[i]; i += 1; estado = 0
                     continue
-                elif linea[i].isdigit():
+                elif es_digito(linea[i]):
                     lexema += linea[i]; i += 1; estado = 1
                 else: break
             elif estado == 1:
-                if linea[i].isdigit():
+                if es_digito(linea[i]):
                     lexema += linea[i]; i += 1
                 else: break
-        if estado == 1 and lexema[-1].isdigit():
+        if estado == 1 and es_digito(lexema[-1]):
             return ('tk_entero', lexema, i-pos)
         return None
 
 class AutomataIdentificador:
     def match(self, linea, pos):
-        if not (linea[pos].isalpha() or linea[pos] == '_'): return None
+        if not (es_letra(linea[pos]) or linea[pos] == '_'): return None
         lexema, i = '', pos
-        while i < len(linea) and (linea[i].isalnum() or linea[i] == '_'):
+        while i < len(linea) and (es_alfnum(linea[i]) or linea[i] == '_'):
             lexema += linea[i]; i += 1
         if lexema in PALABRAS_RESERVADAS:
             return (lexema, lexema, i-pos) # palabra reservada
@@ -82,26 +97,33 @@ class AutomataOpSimple:
             return (OPERADORES_SIMPLES[c], c, 1)
         return None
 
-# --- Analizador principal ---
 class AnalizadorLexico:
-    def __init__(self):
+    def __init__(self,salida="tokens.txt"):
         self.automatas = [
             AutomataCadena(), AutomataOpDoble(), AutomataOpSimple(),
             AutomataEntero(), AutomataIdentificador()
         ]
+        self.salida=salida
+        with open   (self.salida,'w',encoding='utf-8') as f:
+            f.write("")
+    
+    def escribir(self,token):
+        with open(self.salida,'a',encoding='utf-8') as f:
+            f.write(token+'\n')
     def analizar_linea(self, linea, num_linea):
         i = 0
         while i < len(linea):
-            if linea[i].isspace(): i += 1; continue
+            if es_blanco(linea[i]): i += 1; continue
             if linea[i] == '#': break
             for aut in self.automatas:
                 res = aut.match(linea, i)
                 if res:
                     tipo, lexema, longi = res
-                    print(f'<{tipo},{lexema},{num_linea},{i+1}>')
+                    token=f'<{tipo},{lexema},{num_linea},{i+1}>'
+                    self.escribir(token)
                     i += longi; break
             else:
-                print(f'>>> Error léxico(linea:{num_linea},posicion:{i+1})')
+                print(f'>//>//> error lexico(linea:{num_linea},posicion:{i+1})')
                 return
 
     def analizar(self, filename):
@@ -116,5 +138,8 @@ class AnalizadorLexico:
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 2: print('Uso: python analizador_clases.py archivo.py')
-    else: AnalizadorLexico().analizar(sys.argv[1])
+    if len(sys.argv) != 2:
+
+        print('Agregar un archivo para analizar')
+    else: 
+        AnalizadorLexico().analizar(sys.argv[1])
